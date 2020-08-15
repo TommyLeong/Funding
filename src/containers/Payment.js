de1d32b4085795a8446d29102631b24e5393dc1d
Tommy Leong <tommymoleong@gmail.com>
2020-08-15 18:34:15 +0800

    Integrate postAPI to update collected amount in backend

--- src/containers/Payment.js
+++ src/containers/Payment.js
@@ -10,12 +10,64 @@ import { v4 as uuidv4 } from "uuid";
 const Payment = (props) => {
   const history = useHistory();
   const location = useLocation();
-  const { donationAmount, station, donatorName, email } = location.state;
+  const {
+    donationAmount,
+    station,
+    donatorName,
+    email,
+    id,
+    collectedAmount,
+  } = location.state;
 
   let animBox = useRef(null);
   const ackId = uuidv4();
 
+  async function appendIntoDonationRecord() {
+    const data = {
+      station,
+      donatorName,
+      donationAmount,
+      email,
+    };
+
+    let response = await ApiManager.postAPI(
+      "http://localhost:1337/add-donations",
+      data
+    );
+
+    if (response.status === 200) {
+      updateStationDonationAmount();
     }
+  }
+
+  async function updateStationDonationAmount() {
+    const data = {
+      // To append "number" data type, you realize I have added "+" infront of variable
+      // https://stackoverflow.com/a/14496556/4311268
+      collectedAmount: +collectedAmount + +donationAmount,
+    };
+
+    let response = await ApiManager.putAPI(
+      `http://localhost:1337/getallstops/${id}`,
+      data
+    );
+
+    if (response.status === 200) {
+      // Set timeout to simulate waiting response
+      setTimeout(() => {
+        history.push({
+          pathname: UrlPath.acknowledgement,
+          state: {
+            station,
+            donatorName,
+            donationAmount,
+            email,
+            ackUniqueId: ackId,
+          },
+        });
+      }, 3000);
+    }
+  }
 
   useEffect(() => {
     Lottie.loadAnimation({
@@ -26,39 +78,7 @@ const Payment = (props) => {
       animationData: animationData,
     });
 
-    async function postData() {
-      const data = {
-        station,
-        donatorName,
-        donationAmount,
-        email,
-      };
-
-      let response = await ApiManager.postAPI(
-        "http://localhost:1337/add-donations",
-        data
-      );
-
-
-      if (response.status === 200) {
-        // Set timeout to simulate waiting response
-        setTimeout(() => {
-          history.push({
-            pathname: UrlPath.acknowledgement,
-            // .replace(":stationName", busStation)
-            // .replace(":ackUniqueId", ackId)
-            state: {
-              station,
-              donatorName,
-              donationAmount,
-              email,
-              ackUniqueId: ackId,
-            },
-          });
-        }, 3000);
-      }
-    }
-    postData();
+    appendIntoDonationRecord();
   }, []);
 
   const renderLoading = () => {
